5c7b4187b0af84b01afd373600b80fc93b319ad4
Nikolas Cane <nikolas.cane@gmail.com>
2020-03-27 22:29:07 +0200

    Initial Commit

---
+++ AlarmSoundApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  AlarmSoundApp
+//
+//  Created by Nik Cane on 27.03.2020.
+//  Copyright © 2020 Nik Cane. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
